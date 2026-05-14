a79dbad64574a764fa404fbab03001b1ed7798d9
Harry-dev1805 <buiharry21012003@gmail.com>
2026-05-15 00:36:27 +0700

    feat(config): open cors for both nextjs and react-vite, swagger config

---
+++ src/main/java/com/smartpark/swp391/common/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.smartpark.swp391.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    final String securitySchemeName = "bearerAuth";
+
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("SmartPark API")
+                .description("API documentation for SmartPark System")
+                .version("1.0.0"))
+        .servers(List.of(new Server().url("http://localhost:8080").description("Local")))
+        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+  }
+}
