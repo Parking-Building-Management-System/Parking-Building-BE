5dbe919994927b83514a03e6042c3e6e834e4dfc
Harry-dev1805 <buiharry21012003@gmail.com>
2026-05-15 00:46:46 +0700

    feat(config): better override using application config file and environment

--- src/main/java/com/smartpark/swp391/common/config/CorsConfig.java
+++ src/main/java/com/smartpark/swp391/common/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.smartpark.swp391.common.config;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+  @Value("${app.cors.allowed-origins}")
+  private List<String> allowedOrigins;
+
   @Bean
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
@@ -15,7 +20,7 @@ public class CorsConfig {
       public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+            .allowedOrigins(allowedOrigins.toArray(new String[0]))
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
--- src/main/java/com/smartpark/swp391/common/config/SwaggerConfig.java
+++ src/main/java/com/smartpark/swp391/common/config/SwaggerConfig.java
@@ -7,11 +7,18 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+  @Value("${app.swagger.server.url}")
+  private String serverUrl;
+
+  @Value("${app.swagger.server.description}")
+  private String serverDescription;
 
   @Bean
   public OpenAPI customOpenAPI() {
@@ -23,7 +30,11 @@ public class SwaggerConfig {
                 .title("SmartPark API")
                 .description("API documentation for SmartPark System")
                 .version("1.0.0"))
-        .servers(List.of(new Server().url("http://localhost:8080").description("Local")))
+        // Bơm Server list từ ENV vào đây
+        .servers(
+            List.of(
+                new Server().url(serverUrl).description(serverDescription),
+                new Server().url("http://localhost:8080").description("Local Development")))
         .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
         .components(
             new Components()
