0185d08d16767b4e89c6b06f88159dc1a3707804
Huỳnh Đăng Khoa <dangkhoa142005@gmail.com>
2026-05-31 22:32:41 +0700

    feat(staff-exit): add exit lifecycle lookups

--- src/main/java/com/smartpark/swp391/modules/operation/enumType/SessionPaymentStatus.java
+++ src/main/java/com/smartpark/swp391/modules/operation/enumType/SessionPaymentStatus.java
@@ -3,6 +3,8 @@ package com.smartpark.swp391.modules.operation.enumType;
 public enum SessionPaymentStatus {
   PENDING,
   PAID,
+  CASH_COLLECTED,
+  SURCHARGE_COLLECTED,
   CANCELLED,
   EXPIRED,
   FAILED
